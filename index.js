34961c1c76aebb83249baca6086e94fee6e01999
iklimduman <iklimdumann@gmail.com>
2022-07-24 12:30:10 +0300

    connect mongoDB

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://test_user:testuser@cluster0.u9bnl.mongodb.net/testdb?retryWrites=true&w=majority")
+    .then(() => console.log("db connection successull"))
+    .catch(err => console.log(err));
+
+app.listen(5000, () => {
+    console.log("backend server is running")
+});
